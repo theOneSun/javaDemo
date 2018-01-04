06ad5835dca8c7edb1669722d85eeb865966a946
theOneSun <sunjian_better@163.com>
2018-01-04 19:07:06 +0800

    增加整数计算测试

--- src/study/baseTest/IntTest.java
+++ src/study/baseTest/IntTest.java
@@ -12,4 +12,14 @@ public class IntTest
         int a = (int) 1.99999;
         System.out.println(a);
     }
+
+    @Test
+    public void testDiscount() throws Exception
+    {
+        int price = 28900;
+        int value = 85;
+        float v = value / 100f;
+        int discountPrice = (int) (price * v);
+        System.out.println(discountPrice);
+    }
 }
