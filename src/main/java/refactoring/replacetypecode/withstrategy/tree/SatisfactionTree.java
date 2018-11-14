1f6696130d1890c5a353723809bc8ea2773dad6a
theOneSun <sunjian_better@163.com>
2018-11-14 16:41:59 +0800

    增加lombok依赖和git的忽略文件

--- src/main/java/refactoring/replacetypecode/withstrategy/tree/SatisfactionTree.java
+++ src/main/java/refactoring/replacetypecode/withstrategy/tree/SatisfactionTree.java
@@ -1,9 +1,39 @@
 package refactoring.replacetypecode.withstrategy.tree;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.newClass.domain.IndexTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 满意度树
  *
  * @author sunjian.
  */
+@Data
+@NoArgsConstructor
 public class SatisfactionTree {
+    /**
+     * 名称
+     */
+    private String name;
+    /**
+     * 得分
+     */
+    private Double score;
+    /**
+     * 权重
+     */
+    private Double weight;
+    /**
+     * 是否叶子结点
+     */
+    private boolean leaf;
+    /**
+     * 孩子节点集合
+     */
+    private List<IndexTree> children = new ArrayList<>();
 }
+
